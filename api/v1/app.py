6642d5b3fac8497d206adaaac390281b4544f1c4
Bigcujo <princefrancisogbogu@gmail.com>
2024-08-06 21:54:42 +0000

    design 404 error page

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """roor file for the api"""
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 import os
+import MySQLdb
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -29,14 +30,21 @@ def create_database():
     except MySQLdb.Error as err:
         print(f"Error: {err}")
 
-
-
 @app.teardown_appcontext
 def teardown_db(exception):
     """Closes the storage on teardown"""
     storage.close()
 
+
+@app.errorhandler(404)
+def not_found(error):
+    """Handle all 404 errors and customize it to a simple key value pair"""
+    return jsonify({
+        "error": "Not found"
+        }), 404
+
+
 if __name__ == "__main__":
     host = os.getenv("HBNB_API_HOST", "0.0.0.0")
     port = int(os.getenv("HBNB_API_PORT", "5000"))
-    app.run(host=host, port=port, threaded=True)
+    app.run(host=host, port=port, threaded=True, debug=True)
