8630b73a2440dbf4448087fa327374f8c84c79f1
Bigcujo <princefrancisogbogu@gmail.com>
2024-08-09 00:30:34 +0000

    handle get method on the api

---
+++ api/v1/views/states.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+""" create the state method"""
+import models
+from flask import Blueprint, jsonify, request, abort
+from models import storage
+from models.state import State
+from api.v1.views import app_views
+
+api = Blueprint('states', __name__)
+
+@app_views.route('/states', methods=['GET'], strict_slashes=False)
+def get_all_states():
+    states = storage.all(State).values()
+    return jsonify([state.to_dict() for state in states])
+
+@api.route('/api/v1/states/<state_id>', methods=['GET'])
+def get_state(state_id):
+    state = storage.get(State, state_id)
+    if not state:
+        abort(404)
+    return jsonify(state.to_dict())
+
+@api.route('/api/v1/states/', methods=['POST'])
+def create_state():
+    if not request.is_json:
+        abort(400, description="Not a JSON")
+    data = request.get_json()
+    if 'name' not in data:
+        abort(400, description="Missing name")
+    state = State(**data)
+    storage.new(state)
+    storage.save()
+    return jsonify(state.to_dict()), 201
+
+@api.route('/api/v1/states/<state_id>', methods=['PUT'])
+def update_state(state_id):
+    if not request.is_json:
+        abort(400, description="Not a JSON")
+    data = request.get_json()
+    state = storage.get(State, state_id)
+    if not state:
+        abort(404)
+    for key, value in data.items():
+        if key not in ['id', 'created_at', 'updated_at']:
+            setattr(state, key, value)
+    storage.save()
+    return jsonify(state.to_dict())
+
+@api.route('/api/v1/states/<state_id>', methods=['DELETE'])
+def delete_state(state_id):
+    state = storage.get(State, state_id)
+    if not state:
+        abort(404)
+    storage.delete(state)
+    storage.save()
+    return {}, 200
+
