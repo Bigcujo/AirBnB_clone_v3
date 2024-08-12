86671da7c32c257bbb914d28023090b269abc7d3
Bigcujo <princefrancisogbogu@gmail.com>
2024-08-12 20:50:23 +0000

    documnet the create api method

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""root file for the RESTFUL API"""
+"""root file for the app.py"""
 from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
